fb3272e6c176ffcb0b50eaa2ab783a782ef8773c
haseebcheema <ihaseebtariq1@gmail.com>
2024-04-22 23:54:26 +0400

    landing component setup with some animatons

--- src/components/Landing/index.js
+++ src/components/Landing/index.js
@@ -1,8 +1,33 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useRef } from 'react';
 import Image from 'next/image';
 import styles from './style.module.scss';
+import gsap from 'gsap';
 
 function index() {
+
+  const firstText = useRef(null);
+  const secondText = useRef(null);
+  let xPosition = 0;
+  let direction = -1;
+
+  useEffect(() => {
+    requestAnimationFrame(animate);
+  });
+
+  const animate = () => {
+    
+    if(xPosition <= -100){
+      xPosition = 0
+    }
+     
+    gsap.set(firstText.current, {xPercent: xPosition});
+    gsap.set(secondText.current, {xPercent: xPosition});
+    xPosition += 0.1 * direction;
+    requestAnimationFrame(animate);
+  }
+
   return (
     <div>
       <main className={styles.landing}>
@@ -12,9 +37,9 @@ function index() {
         alt="personal-image"
       />
       <div className={styles.slidercontainer}>
-        <div className=''>
-            <p>Freelance Developer</p>
-            <p>Freelance Developer</p>
+        <div className={styles.slider}>
+            <p ref={firstText}>Dennis Snellenberg —</p>
+            <p ref={secondText}>Dennis Snellenberg —</p>
         </div>
       </div>
       </main>
