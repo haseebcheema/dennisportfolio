4341d780d641adf167c6d0e17ca9cdc4623c683b
haseebcheema <ihaseebtariq1@gmail.com>
2024-04-23 16:53:26 +0400

    slight change in the project structure

--- src/components/Header/Nav/index.js
+++ src/components/Header/Nav/index.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import styles from './style.module.scss';
-import { title } from 'process';
-import Link from 'next/link';
+import Link from './Link';
 
 function index() {
 
