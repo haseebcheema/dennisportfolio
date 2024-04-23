a238b48d7ef0d15760edba479a343693e186c24b
haseebcheema <ihaseebtariq1@gmail.com>
2024-04-23 22:06:46 +0400

    few changes in the nav

--- src/components/Header/Nav/Link/index.js
+++ src/components/Header/Nav/Link/index.js
@@ -2,15 +2,19 @@ import React from 'react';
 import styles from './style.module.scss';
 import Link from 'next/link';
 
-function index({ data }) {
+function index({ data, isActive, setSelectedIndicator }) {
+
+  // destructuring the data passed in the form of props
+  const {title, href} = data;
+
   return (
-    <div className={styles.link}>
+    <div onMouseEnter={() => setSelectedIndicator(href)} className={styles.links}>
       <div className={styles.indicator}>
       </div>
 
       {/* accessing the data passed in the form of props */}
-      <Link href={data.href}>
-          {data.title}
+      <Link href={href}>
+          {title}
       </Link>
     </div>
   )
--- src/components/Header/Nav/index.js
+++ src/components/Header/Nav/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styles from './style.module.scss';
 import Link from './Link';
+import { usePathname } from 'next/navigation';
 
 function index() {
+
+    // get the current url or pathname
+    const pathname = usePathname();
+
+    // state to track the selected indicator in the menu
+    const [selectedIndicator, setSelectedIndicator] = useState(pathname);
 
     // array of navigation items
     const navItems = [
@@ -27,15 +34,26 @@ function index() {
   return (
     <div className={styles.menu}>
       <div className={styles.body}>
-        <div className={styles.nav}>
+        <div onMouseLeave={() => {setSelectedIndicator(pathname)}} className={styles.nav}>
             <div className={styles.header}>
                 <h5>Navigation</h5>
             </div>
-            {/* iterating navItems and passing the data in th form of props to the Link component */}
+            {/* iterating navItems and passing the data in the form of props to the Link component */}
             {navItems.map((data, index) => {
-                return <Link className={styles.a} key={index} data={{index, ...data}}/>
+                return <Link key={index} data={{index, ...data}} isActive={selectedIndicator == data.href} setSelectedIndicator={setSelectedIndicator}></Link>
                 })
             }
+        </div>
+        <div className={styles.footer}>
+            <div className={styles.socials}>
+                <h5>Socials</h5>
+            </div>
+            <div className={styles.links}>
+                <a href='https://www.awwwards.com/dennissnellenberg/' target='_blank'>Awwwards</a>
+                <a href='https://www.instagram.com/codebydennis/' target='_blank'>Instagram</a>
+                <a href='https://twitter.com/codebydennis' target='_blank'>Twitter</a>
+                <a href='https://www.linkedin.com/in/dennissnellenberg/' target='_blank'>LinkedIn</a>
+            </div>
         </div>
       </div>
     </div>
